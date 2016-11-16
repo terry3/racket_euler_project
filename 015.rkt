470f64fe8691416c4f0a3a2ed18345a7b60eb8fd
terry3 <terryr3rd@yeah.net>
2016-11-16 23:15:48 +0800

    Add 015.

---
+++ 015.rkt
@@ -0,0 +1,7 @@
+#lang racket
+;; Lattice paths
+;; Starting in the top left corner of a 2×2 grid, and only being able to move
+;; to the right and down, there are exactly 6 routes to the bottom right corner.
+(require math)
+
+(binomial 40 20)
